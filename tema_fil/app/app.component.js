c44bd340004250027e670284e51e08b6c55234a9
Zixian Song <nsong@kth.se>
2016-04-06 20:14:19 +0200

    x

---
+++ tema_fil/app/app.component.js
@@ -0,0 +1,14 @@
+
+
+(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'my-app',
+      template: '<h1>My First tja 2 App</h1>'
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
+
+
